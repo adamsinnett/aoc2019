5eab0d232e005bfb36f960ad5d8d83035dd81440
Adam Sinnett <quandrum@gmail.com>
2019-12-01 21:54:34 -0800

    Initial commit (days 1 and 2 even!)

---
+++ AoC2019.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AoC2019
+{
+    class AdventOFCode
+    {
+        static void Main(string[] args)
+        {
+            DayOne.getFuel();
+            DayTwo.getCodes();
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("Good so far!");
+        }
+
+       
+    }
+}
---
+++ Day1.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace AoC2019
+{
+    class DayOne
+    {
+        public const string path = @"C:\Users\quand\projects\day1.txt";
+        public static void getFuel()
+        {
+            System.Console.WriteLine("Day One");
+
+            var mass = System.IO.File.ReadAllLines(path)
+                .Select(int.Parse)
+                .ToArray();
+
+            var part1 = mass
+                .Select(m => FuelCalculator(m, false))
+                .Aggregate(0, (memo, fuel) => memo + fuel);
+
+            var part2 = mass
+                .Select(m => FuelCalculator(m, true))
+                .Aggregate(0, (memo, fuel) => memo + fuel);
+
+            System.Console.WriteLine("Part 1 is {0}", part1);
+            System.Console.WriteLine("Part 2 is {0}", part2);
+            
+        }
+
+        static int FuelCalculator(int mass, bool accountForFuel)
+        {
+            int fuel = (mass / 3) - 2;
+            if (!accountForFuel) return fuel;
+
+            return (fuel <= 0) ? 0 : fuel + FuelCalculator(fuel, accountForFuel);
+        }
+    }
+}
